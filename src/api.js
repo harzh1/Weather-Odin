edbf79556d49b5e5962eacbf448d735bb7c167ce
Harsh Raj <101712553+harzh1@users.noreply.github.com>
2024-01-09 13:49:14 +0530

    Back to previous version

--- src/api.js
+++ src/api.js
@@ -1,37 +1,20 @@
-const express = require("express");
-const fetch = require("node-fetch");
-const app = express();
-const port = process.env.PORT || 3000;
+export default function api() {
+  const key = "5bc3480b3d8d4997b85112547240501";
 
-const key = "5bc3480b3d8d4997b85112547240501";
+  async function getWeather(city) {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${city}&days=4`,
+      {
+        mode: "cors",
+      }
+    );
+    const data = await response.json();
+    console.log(data);
 
-app.get("/api/weather", async (req, res) => {
-  const { city } = req.query;
-  const url = `http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${city}&days=4`;
-
-  try {
-    const apiRes = await fetch(url);
-    const data = await apiRes.json();
-    res.send(data);
-  } catch (error) {
-    res.status(500).send({ message: error.message });
+    return data;
   }
-});
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
-
-async function getWeather(city) {
-  const response = await fetch(`/api/weather?city=${city}`, {
-    mode: "cors",
-  });
-  const data = await response.json();
-  console.log(data);
-
-  return data;
+  return {
+    getWeather,
+  };
 }
-
-module.exports = {
-  getWeather,
-};
